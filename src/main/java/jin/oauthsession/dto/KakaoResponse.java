6a77825998c2ee4d6c722154af6753be36224d66
Jinseonghyun <jinsunghyun9@gmail.com>
2024-08-09 14:50:10 +0900

    카카오 dto 에서 이름 받아오는 메서드 수정

--- src/main/java/jin/oauthsession/dto/KakaoResponse.java
+++ src/main/java/jin/oauthsession/dto/KakaoResponse.java
@@ -5,30 +5,32 @@ import java.util.Map;
 public class KakaoResponse implements OAuth2Response {
 
     private final Map<String, Object> attributes;
+    private final Map<String, Object> properties;
+    private final Map<String, Object> kakaoAccount;
 
-    // 생성자를 통해서 위의 해당 attributes를 초기화 위한 Map 데이터를 받는다.
     public KakaoResponse(Map<String, Object> attributes) {
-        this.attributes = (Map<String, Object>) attributes.get("response"); // 네이버의 경우 데이터 내부에 또 데이터가 존재 하기에 (Map<String, Object>) 캐스팅하고.get 사용해서 꺼내준다.
+        this.attributes = attributes;
+        this.properties = (Map<String, Object>) attributes.get("properties");
+        this.kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
     }
-
 
     @Override
     public String getProvider() {
-        return "kakao"; // 네이버에 대한 답이기 때문에
+        return "kakao";
     }
 
     @Override
     public String getProviderId() {
-        return attributes.get("id").toString(); // id 는 attributes 에서 id 키를 꺼낸다.toString 으로 스트링을 응답한다.
+        return attributes.get("id").toString();
     }
 
     @Override
     public String getEmail() {
-        return attributes.get("email").toString(); // email 은 attributes 에서 email 키를 꺼낸다.toString 으로 스트링을 응답한다.
+        return (String) kakaoAccount.get("email");
     }
 
     @Override
     public String getName() {
-        return attributes.get("nickname").toString(); // name 은 attributes 에서 name 키를 꺼낸다.toString 으로 스트링을 응답한다.
+        return (String) properties.get("nickname");
     }
 }
